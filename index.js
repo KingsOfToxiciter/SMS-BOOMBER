0f21e01eca5b072109765a81262e79fb92ffd677
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-06-25 22:54:34 +0600

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { boomber } = require("./utils")
 
 const app = express();
-const port = 3000;
+const port = 3000; 
 
 app.get("/sms-boomber", async (req, res) => {
   const number = req.query.number;
