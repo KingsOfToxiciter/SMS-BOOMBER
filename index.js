1b1394dab99cca8ee5617d28808e548b65b8915f
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-06-25 13:37:49 +0600

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { boomber } = require("./utils")
 const app = express();
 const port = 3000;
 
-app.get("/msgboomber", async (req, res) => {
+app.get("/sms-boomber", async (req, res) => {
   const number = req.query.number;
   const limit = req.query.limit || 500;
     if (!number) {
